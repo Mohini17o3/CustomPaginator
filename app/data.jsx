a4e12809392a736e0442f4e7cf5b0b5307de5f3c
Mohini <upretimohini@gmail.com>
2024-04-22 07:06:13 +0530

    data configure

---
+++ app/data.jsx
@@ -0,0 +1,14 @@
+const data = [
+    { id: 1, date: "2024-04-20", fileName: "Document1.pdf" },
+    { id: 2, date: "2024-04-19", fileName: "Document2.docx" },
+    { id: 3, date: "2024-04-18", fileName: "Document3.txt" },
+    { id: 4, date: "2024-04-17", fileName: "Document4.pdf" },
+    { id: 5, date: "2024-04-16", fileName: "Document5.docx" },
+    { id: 6, date: "2024-04-15", fileName: "Document6.txt" },
+    { id: 7, date: "2024-04-14", fileName: "Document7.pdf" },
+    { id: 8, date: "2024-04-13", fileName: "Document8.docx" },
+    { id: 9, date: "2024-04-12", fileName: "Document9.txt" },
+    { id: 10, date: "2024-04-11", fileName: "Document10.pdf" },
+    
+  ];
+  
